3e92063bf3fe346dfd25d72f4f6bc98391745e06
sumi77799 <103524670+sumi77799@users.noreply.github.com>
2022-06-04 22:02:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+
+
+console.log("Tic Tack Toe")
+let music= new Audio("music.mp3")
+let audioTurn=new Audio("ting.mp3");
+let gameover=new Audio("gameover.mp3");
+
+
+
+
+
+
+let turn ="X"
+let isgameover=false;
+//Funtion to change the turn
+const changeTurn=()=>{
+  return turn ==="X"? "0": "X"
+
+}
+
+//Function to check for a win
+
+const checkWin =()=>{
+let boxtext=document.getElementsByClassName("boxtext");
+let wins=[
+    [0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]]
+wins.forEach(e=>{
+    if((boxtext[e[0]].innerText===boxtext[e[1]].innerText) && (boxtext[e[2]].innerText===boxtext[e[1]].innerText) &&(boxtext[e[0]].innerText !=="")){
+        document.querySelector(".info").innerText=boxtext[e[0]].innerText+"  Wins The Game"
+        isgameover=true;
+      document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width="300px"
+      gameover.play();
+     
+      
+    };
+})
+}
+
+
+//Game Logic
+
+
+
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element =>{
+    let boxtext =element.querySelector(".boxtext");
+    setTimeout(function(){
+        music.play()
+     },1000)
+
+    element.addEventListener("click" ,(e)=>{
+        if(boxtext.innerText===""){
+        boxtext.innerText= turn;
+        turn=changeTurn();
+        audioTurn.play();
+        checkWin();
+        if(!isgameover){
+            document.getElementsByClassName("info")[0].innerText="Turn for "+ turn;
+        }
+       
+        }
+    })
+
+})
+
+
+//add onclick to reset button
+
+reset.addEventListener("click",()=>{
+    let boxtext=document.querySelectorAll(".boxtext");
+    Array.from(boxtext).forEach(element =>{
+        element.innerText=""
+    });
+    turn="X";
+   isgameover=false;
+        document.getElementsByClassName("info")[0].innerText="Turn for "+ turn;
+        document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width="0"
+})
